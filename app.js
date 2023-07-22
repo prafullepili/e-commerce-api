f87e8127041130fb15e36d38dce0f79eaa3b5e6d
Prafull Epili <prafullepili@gmail.com>
2023-07-23 00:45:23 +0530

    not-fount and error handler added

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express')
+require('dotenv').config();
+require('express-async-errors');
 const app = express()
 
 // database
-require('dotenv').config();
 const connectDB = require('./db/connect');
+
+//middleware 
+const notFountMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+app.use(express.json());
+app.get('/', (req, res) => {
+    req.send('E-commerce api')
+})
+
+app.use(notFountMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
 const start = async () => {
