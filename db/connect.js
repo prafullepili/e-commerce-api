e1360ffe57866bdef1047579739ccc17931bdaaf
Prafull Epili <prafullepili@gmail.com>
2023-07-23 00:57:57 +0530

    bug fix

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', true);
-const connectDB = (url) => {
-  return mongoose.connect(url, (err) => err ? console.log(err) : console.log("db connected"))
+const connectDB = async (url) => {
+  return mongoose.connect(url, {}, (err) => err ? console.log(err) : console.log("db connected"))
 }
 module.exports = connectDB
