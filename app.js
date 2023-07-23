c22afaab423d54c030d959c62b4ac10f90df1545
Prafull Epili <prafullepili@gmail.com>
2023-07-23 17:55:37 +0530

    loginRoute and secure,signedCookie

--- app.js
+++ app.js
@@ -16,11 +16,12 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(morgan('tiny'))
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser(process.env.JWT_SECRET))
 
 
 app.get('/', (req, res) => {
-    console.log(req.cookies)
+    // console.log(req.cookies)
+    console.log(req.signedCookies)
     res.send('E-commerce api')
 })
 app.use('/api/v1/auth', authRouter)
