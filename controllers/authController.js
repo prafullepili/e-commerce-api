d0b5a7e83ebfc4f3e5a6ef8ecab285d0421bb414
Prafull Epili <prafullepili@gmail.com>
2023-07-23 12:13:39 +0530

    jwt token created

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,21 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
+const jwt = require('jsonwebtoken')
 
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
 
     const isEmailExist = await User.findOne({ email })
-    if (isEmailExist)
+    if (isEmailExist) {
         throw new CustomError.BadRequestError('Email already exists')
+    }
     const user = await User.create({ name, email, password });
-    res.status(StatusCodes.CREATED).json({ user })
+    const tokenUser = { name: user.name, userId: user._id, role: user.role }
+
+    const token = jwt.sign(tokenUser, 'jwtSecret', { expiresIn: '1d' })
+    res.status(StatusCodes.CREATED).json({ user: tokenUser, token })
 }
 
 const login = async (req, res) => {
