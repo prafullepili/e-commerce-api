722768e31818c2dfddd0779b0c3583df66166f78
Prafull Epili <prafullepili@gmail.com>
2023-07-23 23:35:45 +0530

    showCurrentUser done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const { authenticateUser, authorizePermissions } = require('../middleware/authentication')
 
 router.route('/').get(authenticateUser, authorizePermissions('admin'), getAllUsers)
-router.route('/showMe').get(showCurrentUser)
+router.route('/showMe').get(authenticateUser, showCurrentUser)
 router.route('/updateUser').patch(updateUser)
 router.route('/updateUserPassword').patch(updateUserPassword)
 router.route('/:id').get(authenticateUser, authorizePermissions('admin'), getSingleUser)
