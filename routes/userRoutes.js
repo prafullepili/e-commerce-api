3dbcce7191aa1c6859031be435ceee3e3bf7650d
Prafull Epili <prafullepili@gmail.com>
2023-07-23 20:43:34 +0530

    authorizePermissions meddleware done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,13 +5,13 @@ const { getAllUsers,
     updateUser,
     updateUserPassword } = require('../controllers/userController')
 const router = express.Router();
-const { authenticateUser } = require('../middleware/authentication')
+const { authenticateUser, authorizePermissions } = require('../middleware/authentication')
 
-router.route('/').get(authenticateUser, getAllUsers)
+router.route('/').get(authenticateUser, authorizePermissions, getAllUsers)
 router.route('/showMe').get(showCurrentUser)
 router.route('/updateUser').patch(updateUser)
 router.route('/updateUserPassword').patch(updateUserPassword)
-router.route('/:id').get(authenticateUser, getSingleUser)
+router.route('/:id').get(authenticateUser, authorizePermissions, getSingleUser)
 
 
 module.exports = router
