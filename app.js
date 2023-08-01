95a071a05a4ea4bf8161a5452a490a1989f9db67
Prafull Epili <prafullepili@gmail.com>
2023-08-02 00:59:32 +0530

    review route setup done

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const connectDB = require('./db/connect'); // database
 const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
 const productRouter = require('./routes/productRoutes')
+const reviewRouter = require('./routes/reviewRoutes')
 
 //middleware 
 const notFountMiddleware = require('./middleware/not-found') //page not found
@@ -33,6 +34,7 @@ app.get('/', (req, res) => {
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/products', productRouter)
+app.use('/api/v1/reviews', reviewRouter)
 
 
 app.use(notFountMiddleware)
---
+++ routes/reviewRoutes.js
@@ -0,0 +1,23 @@
+const { createReview,
+    getAllReviews,
+    getSingleReview,
+    updateReview,
+    deleteReview } = require('../controllers/reviewController')
+const { authenticateUser, authorizePermissions } = require('../middleware/authentication')
+const express = require('express');
+
+const router = express.Router();
+
+router
+    .route('/')
+    .get(getAllReviews)
+    .post(authenticateUser, createReview)
+
+router
+    .route('/:id')
+    .get(getSingleReview)
+    .patch(authenticateUser, updateReview)
+    .delete(authenticateUser, deleteReview)
+
+
+module.exports = router;
