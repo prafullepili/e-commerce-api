b553d7f37cce9684fd0a9ccf06a6f81589d3148e
Prafull Epili <prafullepili@gmail.com>
2023-08-01 18:09:25 +0530

    Product controller and route setup done

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const connectDB = require('./db/connect'); // database
 //routers
 const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
+const productRouter = require('./routes/productRoutes')
 
 //middleware 
 const notFountMiddleware = require('./middleware/not-found') //page not found
@@ -27,6 +28,7 @@ app.get('/', (req, res) => {
 })
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
+app.use('/api/v1/products', productRouter)
 
 
 app.use(notFountMiddleware)
---
+++ routes/productRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const {
+    createProduct,
+    getAllProducts,
+    getSingleProduct,
+    updateProduct,
+    deleteProduct,
+    uploadImage } = require('../controllers/productController');
+const router = express.Router();
+const {
+    authenticateUser,
+    authorizePermissions } = require('../middleware/authentication')
+
+router.route('/')
+    .get(getAllProducts)
+    .post([authenticateUser, authorizePermissions('admin')], createProduct)
+
+router.route('/uploadImage').post([authenticateUser, authorizePermissions('admin')], uploadImage)
+
+router.route('/:id')
+    .get(getSingleProduct)
+    .patch([authenticateUser, authorizePermissions('admin')], updateProduct)
+    .delete([authenticateUser, authorizePermissions('admin')], deleteProduct)
+
+
+module.exports = router
