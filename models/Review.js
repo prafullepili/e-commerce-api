8a2f35714e8237168d2f031a6b478c8c5bc2ee78
Prafull Epili <prafullepili@gmail.com>
2023-08-02 23:55:20 +0530

    createReview done

--- models/Review.js
+++ models/Review.js
@@ -18,7 +18,7 @@ const ReviewSchema = new mongoose.Schema({
 
     comment: {
         type: String,
-        required: [true, 'Please provide review text']
+        required: [true, 'Please provide review comment']
     },
 
     user: {
