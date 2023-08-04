28a98069980e22d781ea1a38ab7a0561820a287f
Prafull Epili <prafullepili@gmail.com>
2023-08-04 11:06:53 +0530

    db local setup

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(errorHandlerMiddleware)
 const port = process.env.PORT || 5000
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URI)
+        await connectDB(process.env.MONGO_URI_LOCAL)
         app.listen(port, console.log(`Server is listening on port ${port}...`))
     } catch (error) {
         console.log(error);
