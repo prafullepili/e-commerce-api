128b17a07a805c61ab3d2d9428df41710c6f4577
Prafull Epili <prafullepili@gmail.com>
2023-08-06 07:04:34 +0530

    access revoked now admin can not delete/update reviews

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -1,11 +1,11 @@
 const CustomError = require('../errors')
 
-const checkPermissions = (requestUser, resouceUserId) => {
+const checkPermissions = (requestUser, resouceUserId, adminFlag = 0) => {
     // console.log(4, requestUser); //{ name: 'Prafull', userId: '64bd8876c98d8fab174fb975', role: 'admin' }
     // console.log(5, resouceUserId); //new ObjectId("64bdfd5c7e4a5df255abedee")
     // console.log(6, typeof resouceUserId); //object
 
-    if (requestUser.role === 'admin') return;
+    if (adminFlag) if (requestUser.role === 'admin') return;
     if (requestUser.userId === resouceUserId.toString()) return
     throw new CustomError.UnauthorizedError('Not authorized to access this route')
 }
