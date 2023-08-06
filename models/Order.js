ed259f5b7e3f8e617ddeeb311e722370c0d2a221
Prafull Epili <prafullepili@gmail.com>
2023-08-06 19:02:19 +0530

    changed cartItems to orderItems

--- models/Order.js
+++ models/Order.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const SingleCartItemSchema = mongoose.Schema({
+const SingleOrderItemSchema = mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -42,7 +42,7 @@ const OrderSchema = mongoose.Schema({
         type: Number,
         required: true,
     },
-    cartItems: [SingleCartItemSchema],
+    orderItems: [SingleOrderItemSchema],
     status: {
         type: String,
         enum: ['pending', 'failed', 'paid', 'delivered', 'canceled'],
