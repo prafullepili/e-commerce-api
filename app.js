042714927c64935cd9c45c10078c86d3785b0ef8
Prafull Epili <prafullepili@gmail.com>
2023-08-06 08:38:30 +0530

    Route define for orders

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
 const productRouter = require('./routes/productRoutes')
 const reviewRouter = require('./routes/reviewRoutes')
+const orderRouter = require('./routes/orderRoutes')
 
 //middleware 
 const notFountMiddleware = require('./middleware/not-found') //page not found
@@ -35,6 +36,7 @@ app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/products', productRouter)
 app.use('/api/v1/reviews', reviewRouter)
+app.use('/api/v1/orders', orderRouter)
 
 
 app.use(notFountMiddleware)
